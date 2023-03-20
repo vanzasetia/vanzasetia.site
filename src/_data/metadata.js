cd82825e84a69c13a39a57d8bbade8007213c72d
Vanza Setia <venusbumi2@gmail.com>
2023-03-20 14:52:34 +0700

    Update the metadata URL

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "Blog Posts of Vanza Setia",
-  url: "https://vanzasetia.netlify.app",
+  url: "https://vanzasetia.site",
   language: "en",
   description:
     "Here you can find my blog posts about web accessibility, my learning experiences, and other things related to front-end web development.",
