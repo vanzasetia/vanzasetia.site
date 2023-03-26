4b31288b609410715bd78a37911217d24fb77a4d
Vanza Setia <venusbumi2@gmail.com>
2023-03-26 11:55:43 +0700

    Repurpose the home page
    This commit makes the home page as a way to help myself getting hired as
    a full-time frontend developer.

---
+++ src/_data/projects.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    title: "Designo",
+    live: "https://officialdesigno.netlify.app/",
+    source: "https://github.com/vanzasetia/designo-multi-page-website",
+    descriptions: [
+      "This is a responsive and accessible website with a total of seven pages. It has a hamburger menu and a contact form. Both work with and without JavaScript.",
+      "This is also a challenge with the most difficult level (Guru level). I have written eight things I learned while building this project."
+    ]
+  },
+  {
+    title: "My Team",
+    live: "https://myteamsoftware.netlify.app/",
+    source: "https://github.com/vanzasetia/myteam-multi-page-website",
+    descriptions: [
+      "This is a fully functional three-page website. It has a hamburger menu for mobile users. It's a responsive website with fluid typography and spacing.",
+      "I built the site with progressive enhancement in mind. Meaning, the site also works without JavaScript."
+    ]
+  },
+  {
+    title: "Dev Finder",
+    live: "https://officialdevfinder.netlify.app/",
+    source: "https://github.com/vanzasetia/github-user-search-app",
+    descriptions: [
+      "This website has a dark mode and a light mode. It also adapts to the user's preferred color scheme. It has a search feature to find a GitHub user.",
+      "This challenge made me learn how to create an accessible theme switcher. I also learned about asynchronous programming."
+    ]
+  },
+  {
+    title: "Pricing Component (Latest)",
+    live: "https://sitepricingcomponent.netlify.app/",
+    source: "https://github.com/vanzasetia/pricing-component-with-toggle",
+    descriptions: [
+      "This is a pricing component with a toggle button. I make sure that the toggle button is accessible to keyboard and screen readers.",
+      "I tried to build the website progressively. It means the CSS and the JavaScript is only for enhancing the experience. Without those things, the site should be able to function. In short, the site will still make sense with just HTML."
+    ]
+  }
+];
