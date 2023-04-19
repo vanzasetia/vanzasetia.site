d14c6a46f61c74cbc93f4a70db71f05de2351b3f
Vanza Setia <venusbumi2@gmail.com>
2023-04-19 09:53:20 +0700

    Change the format of the resource lists
    This commit uses heading level three and paragraph element instead of a
    list element. Also, the data is available on a single file to make it
    easier to add and remove links.
    
    This commit also removes the date since I do not see any benefit. I am
    also lazy updating the date.

---
+++ src/_data/resources.js
@@ -0,0 +1,308 @@
+module.exports = {
+  knowledge: [
+    {
+      title: "FED Mentor",
+      description: `Grace Snow is an accessibility expert who writes amazing blog posts that can be easily understood by beginners. I highly recommend reading her blog posts.`,
+      url: "https://fedmentor.dev/"
+    },
+    {
+      title: "Solid Start",
+      description: `This is a good website for everyone that getting started to learn about web accessibility. It also provides some great resources.`,
+      url: "https://www.solidstart.info/"
+    },
+    {
+      title: "A11y Coffee",
+      description: `It is a friendly website that teaches web accessibility, starting from "What is 'a11y'?". You can read the article while enjoying a cup of coffee.`,
+      url: "https://a11y.coffee/"
+    },
+    {
+      title: "Accessibility Myths",
+      description: `It is a project debunking common accessibility myths. There are 22 myths that have been debunked.`,
+      url: "https://a11ymyths.com/"
+    },
+    {
+      title: "Inclusive Components",
+      description: `This is my first go-to resource when I want to create an accessible component.`,
+      url: "https://inclusive-components.design/"
+    },
+    {
+      title: "Patterns | APG | WAI | W3C",
+      description: `It is like "Inclusive Components". It contains various examples to create accessible components.`,
+      url: "https://www.w3.org/WAI/ARIA/apg/patterns/"
+    },
+    {
+      title: "Axe HTML 4.5 rules",
+      description: `This contains rules to follow to create accessible HTML. Each rule has its article to explain how to fix the problem. I recommend bookmarking it.`,
+      url: "https://dequeuniversity.com/rules/axe/4.5/"
+    },
+    {
+      title: "Scott O'Hara - Blog Posts",
+      description: `Scott is an accessibility engineer at Microsoft. He has written a lot of blog posts about web accessibility since 2013.`,
+      url: "https://www.scottohara.me/"
+    },
+    {
+      title: "Andy Bell (Piccalilli) - Blog Posts",
+      description: `His blog posts are about web accessibility and other topics that are related to web development such as CSS, progressive enhancement, and much more.`,
+      url: "https://piccalil.li/"
+    },
+    {
+      title: "Adrian Roselli — Consultant, Writer, Speaker",
+      description: `Adrian is an accessibility expert who has been writing blogs since before I was born. I recommend exploring his website to learn more about web accessibility.`,
+      url: "https://adrianroselli.com/"
+    },
+    {
+      title: "Checklist - The A11y Project",
+      description: `This contains practical things to do a manual accessibility audit.`,
+      url: "https://www.a11yproject.com/checklist/"
+    },
+    {
+      title: "Posts - The A11Y Project",
+      description: `The A11Y Project has a lot of amazing blog posts about web accessibility.`,
+      url: "https://www.a11yproject.com/posts/"
+    },
+    {
+      title: "Resources - The A11Y Project",
+      description: `I also recommend checking the resources page.`,
+      url: "https://www.a11yproject.com/resources/"
+    },
+    {
+      title: "Web Content Accessibility Guidelines (WCAG) 2.1",
+      description: `The WCAG is a set of technical standards developed by the World Wide Web Consortium (W3C) to provide guidelines for making web content accessible to users with disabilities.`,
+      url: "https://www.w3.org/TR/WCAG21/"
+    },
+    {
+      title: "How to Meet WCAG (Quickref Reference) 2.1",
+      description: `I did a manual accessibility using this.`,
+      url: "https://www.w3.org/WAI/WCAG21/quickref/"
+    },
+    {
+      title: "Colour Blind Awareness",
+      description: `You can learn about different types of color blindness.`,
+      url: "https://www.colourblindawareness.org/"
+    }
+  ],
+  videos: [
+    {
+      title:
+        "How To Get Hired as a Web Developer & WOW Hiring Managers + much more | AMA with Matt Studdert - YouTube",
+      description: `In this video, Matt is answering some incredible questions. I like his answer for the "how do you choose what to focus on learning?". He said that you should just focus on the next thing. For me, that answer put my worries to rest.`,
+      url: "https://www.youtube.com/watch?v=EVrijj32mAc"
+    },
+    {
+      title:
+        "Selfish Accessibility | Adrian Roselli | Talks at Google - YouTube",
+      description: `This is a great video to help you aware of web accessibility. Also, you will learn different situations of users that want to access the web. Plus, how to do manual testing to check the accessibility of your website. I highly recommend watching it.`,
+      url: "https://www.youtube.com/watch?v=XMAksFzFh9E"
+    },
+    {
+      title: "Manuel Matuzović - Lost in Translation - YouTube",
+      description: `HTML is not as easy as you think.`,
+      url: "https://www.youtube.com/watch?v=Wno1IhEBTxc"
+    },
+    {
+      title:
+        "Andy Bell – Be the browser’s mentor, not its micromanager - YouTube",
+      description: `A great talk by Andy Bell that will tell you that good HTML markup can be a strong foundation for your website. Then, how to write modern CSS with progressive enhancement.`,
+      url: "https://www.youtube.com/watch?v=5uhIiI9Ld5M"
+    },
+    {
+      title: "Stephanie Eeckles - Scaling CSS Layout Beyond Pixels - YouTube",
+      description: `Another great talk that covers modern CSS. I recommend watching it to take your CSS skill to the next level.`,
+      url: "https://www.youtube.com/watch?v=8slZJrTK3nE"
+    },
+    {
+      title: "Resilient CSS: 7-part Series - YouTube",
+      description: `This series created by Jen Simmons taught me different ways to write CSS that will fail nicely. Also, a guide to using the "Can I Use" website to help me decide how I craft my CSS.`,
+      url: "https://www.youtube.com/playlist?list=PLbSquHt1VCf1kpv9WRGMCA9_Nn4vCLZ9Y"
+    },
+    {
+      title:
+        "Writing Less Damned Code – Heydon Pickering – btconfBER2016 - YouTube",
+      description: `Less is less. Less code means less problems, less issues, and more time to spend for other things in life.`,
+      url: "https://www.youtube.com/watch?v=tzfHlEFd2Fk"
+    },
+    {
+      title:
+        "Heydon Pickering - Why Inclusive Design is like cooking a curry - YouTube",
+      description: `Learning web accessibility can be like learning to cook a curry.`,
+      url: "https://www.youtube.com/watch?v=VavUglhLH8U"
+    }
+  ],
+  tools: [
+    {
+      title: "W3C HTML Validator",
+      description: `Accessible website starts with valid HTML. So, make sure that your website has valid HTML.`,
+      url: "https://validator.w3.org/"
+    },
+    {
+      title: "The W3C CSS Validation Service",
+      description: `Valid CSS is also important to ensure that the styling of your site works as expected.`,
+      url: "https://jigsaw.w3.org/css-validator/"
+    },
+    {
+      title: "W3C Feed Validation Service, for Atom and RSS",
+      description: `I use this tool to validate my RSS feed.`,
+      url: "https://validator.w3.org/feed/"
+    },
+    {
+      title: "W3C Link Checker",
+      description: `Make sure that you have valid links on your site.`,
+      url: "https://validator.w3.org/checklink/"
+    },
+    {
+      title: "Schema Validator",
+      description: `Validate your schema data.`,
+      url: "https://validator.schema.org/"
+    },
+    {
+      title: "Lighthouse",
+      description: `I recommend using this as the first automatic testing tool for your site. It can catch some accessibility issues and do different types of tests too.`,
+      url: "https://developer.chrome.com/docs/lighthouse/"
+    },
+    {
+      title: "PageSpeed Insights",
+      description: `You can use this website to share with people about your Lighthouse score.`,
+      url: "https://pagespeed.web.dev/"
+    },
+    {
+      title: "Squoosh",
+      description: `Optimize your images so that your users will not be frustrated with their cellular data usage.`,
+      url: "https://squoosh.app/"
+    },
+    {
+      title: "Free Design Tool: Presentations, Video, Social Media | Canva",
+      description: `I use this to design the banners for my READMEs and favicons.`,
+      url: "https://www.canva.com/"
+    },
+    {
+      title: "Grammarly: Free Online Writing Assistant",
+      description: `This tool prevents me from doing typos in my writing.`,
+      url: "https://www.grammarly.com/"
+    },
+    {
+      title: "Favicon Generator for perfect icons on all browsers",
+      description: `I use this tool to generate favicons for this website.`,
+      url: "https://realfavicongenerator.net/"
+    },
+    {
+      title: "RegExr: Learn, Build, & Test RegEx",
+      description: `Practice writing regular expression easily.`,
+      url: "https://regexr.com/"
+    },
+    {
+      title: "CloudConvert",
+      description: `I usually use this website to convert TTF (TrueType) to WOFF2 (Web Open Font Format 2).`,
+      url: "https://cloudconvert.com/"
+    },
+    {
+      title: "SVGOMG - SVGO's Missing GUI",
+      description: `Optimize your SVG files by removing unnecessary code.`,
+      url: "https://jakearchibald.github.io/svgomg/"
+    },
+    {
+      title: "Colorable",
+      description: `Compare two colors to see the contrast of them.`,
+      url: "https://colorable.jxnblk.com/"
+    },
+    {
+      title: "Hemingway Editor",
+      description: `It makes my writing clear and bold by highlighting potential words that can be removed.`,
+      url: "https://hemingwayapp.com/"
+    },
+    {
+      title: "Caninclude",
+      description: `If you get confused about whether anchor tag is valid to be a child element of the code element, then you can use this tool to verify that.`,
+      url: "https://caninclude.glitch.me/"
+    },
+    {
+      title: "Can I use... Support tables for HTML5, CSS3, etc",
+      description: `Check the browser's compatibility with the features that you want to use.`,
+      url: "https://caniuse.com/"
+    },
+    {
+      title: "Can I DevTools?",
+      description: `Check whether or not the features are available in the browser developer tools. For example, I can know that only the developer tool of Safari browser that can count CSS specificity of a selector.`,
+      url: "https://www.canidev.tools/"
+    },
+    {
+      title: "Accessibility Support",
+      description: `Check the compatibility of screen readers with the ARIA attributes you want to use.`,
+      url: "https://a11ysupport.io/"
+    },
+    {
+      title: "Mailtolink.me | The Mailto Link Generator",
+      description: `Generate a mailto link easily.`,
+      url: "https://mailtolink.me/"
+    }
+  ],
+  screenReaders: [
+    {
+      title: "Screen Reader Basics: NVDA -- A11ycasts #09 - YouTube",
+      description: `Learn how to use NVDA screen reader on Windows.`,
+      url: "https://www.youtube.com/watch?v=Jao3s_CwdRU"
+    },
+    {
+      title: "Assistive Tech: TalkBack - YouTube",
+      description: `Learn the basics of navigating with TalkBack.`,
+      url: "https://www.youtube.com/watch?v=0Zpzl4EKCco"
+    },
+    {
+      title: "Complete guide to Narrator - Microsoft Support",
+      description: `Everything you need to know to start using Narrator.`,
+      url: "https://support.microsoft.com/en-us/windows/complete-guide-to-narrator-e4397a0d-ef4f-b386-d8ae-c172f109bdb1"
+    },
+    {
+      title: "Assistive Tech: VoiceOver on iOS - YouTube",
+      description: `Learn to get started to use VoiceOver on iOS devices.`,
+      url: "https://www.youtube.com/watch?v=bCHpdjvxBws"
+    },
+    {
+      title: "Screen Reader Basics: VoiceOver -- A11ycasts #07 - YouTube",
+      description: `Learn to use the free screen reader built into the Mac.`,
+      url: "https://www.youtube.com/watch?v=5R-6WvAihms"
+    },
+    {
+      title: "Screen Reader Keyboard Shortcuts and Gestures",
+      description: `List of resources for various screen readers like Narrator, TalkBack, NVDA, and JAWS.`,
+      url: "https://dequeuniversity.com/screenreaders/"
+    },
+    {
+      title: "Using NVDA to Evaluate Web Accessibility",
+      description: `This is a great article to help you learn the basics of NVDA.`,
+      url: "https://webaim.org/articles/nvda/"
+    }
+  ],
+  games: [
+    {
+      title: "HTML Tags Memory Test",
+      description: `The last time I played this, I think I got around 50 HTML elements. Can you beat mine?`,
+      url: "https://codepen.io/plfstr/full/zYqQeRw"
+    },
+    {
+      title: "TypeRacer - Play Typing Games and Race Friends",
+      description: `This is a place for me to type something other than code.`,
+      url: "https://play.typeracer.com/"
+    },
+    {
+      title: "dulvui/ball2box: Casual ball game created with Godot Engine.",
+      description: `A game that has a purpose to put a ball inside a box. I have played 30 levels and got all three stars.`,
+      url: "https://github.com/dulvui/ball2box"
+    },
+    {
+      title: "Anuken/Mindustry: The automation tower defense RTS",
+      description: `An interesting tower defense where you have to gather resources and defense your base.`,
+      url: "https://github.com/Anuken/Mindustry"
+    },
+    {
+      title:
+        "agateau/pixelwheels: A top-down retro racing game for PC (Linux, macOS, Windows) and Android.",
+      description: `A great racing game with a simple controller and running smoothly on my low-end device. No in-app purchase. Meaning you can get all the cars by playing the game.`,
+      url: "https://github.com/agateau/pixelwheels"
+    },
+    {
+      title: "mjaun/android-anuto: Another ugly tower defense for Android",
+      description: `It has a simple graphic. It has a small file size, only 6MB.`,
+      url: "https://github.com/mjaun/android-anuto"
+    }
+  ]
+};
--- src/resources.md
+++ src/resources.md
@@ -6,94 +6,49 @@ eleventyNavigation:
   key: Resources
   order: 2
 tableOfContents: true
-date: 2023-04-04
 ---
 
 ## Knowledge
 
 You need to have some knowledge first before you can create something. This is a list of trustworthy resources to get some knowledge.
 
-- [FED Mentor](https://fedmentor.dev/) - Grace Snow is an accessibility expert who writes amazing blog posts that can be easily understood by beginners. I highly recommend reading her blog posts.
-- [Solid Start](https://www.solidstart.info/) - This is a good website for everyone that getting started to learn about web accessibility. It also provides some great resources.
-- [A11y Coffee](https://a11y.coffee/) - It is a friendly website that teaches web accessibility, starting from "What is 'a11y'?". You can read the article while enjoying a cup of coffee.
-- [Accessibility Myths](https://a11ymyths.com/) - It is a project debunking common accessibility myths. There are 22 myths that have been debunked.
-- [Inclusive Components](https://inclusive-components.design/) - This is my first go-to resource when I want to create an accessible component.
-- [Patterns | APG | WAI | W3C](https://www.w3.org/WAI/ARIA/apg/patterns/) - It is like "Inclusive Components". It contains various examples to create accessible components.
-- [Axe HTML 4.5 rules](https://dequeuniversity.com/rules/axe/4.5/) - This contains rules to follow to create accessible HTML. Each rule has its article to explain how to fix the problem. I recommend bookmarking it.
-- [Scott O'Hara - Blog Posts](https://www.scottohara.me/) - Scott is an accessibility engineer at Microsoft. He has written a lot of blog posts about web accessibility since 2013.
-- [Andy Bell (Piccalilli) - Blog Posts](https://piccalil.li/) - His blog posts are about web accessibility and other topics that are related to web development such as CSS, progressive enhancement, and much more.
-- [Adrian Roselli — Consultant, Writer, Speaker](https://adrianroselli.com/) - Adrian is an accessibility expert who has been writing blogs since before I was born. I recommend exploring his website to learn more about web accessibility.
-- [Checklist - The A11y Project](https://www.a11yproject.com/checklist/) - This contains practical things to make sure your website is accessible.
-- [Posts - The A11Y Project](https://www.a11yproject.com/posts/) - The A11Y Project has a lot of amazing blog posts about web accessibility. I also recommend checking the resources page — [Resources - The A11Y Project](https://www.a11yproject.com/resources/).
-- [Web Content Accessibility Guidelines (WCAG) 2.1](https://www.w3.org/TR/WCAG21/) - The WCAG is a set of technical standards developed by the World Wide Web Consortium (W3C) to provide guidelines for making web content accessible to users with disabilities.
-- [Understanding WCAG 2.1 | WAI | W3C](https://www.w3.org/WAI/WCAG21/Understanding/) - This is a much more friendly user interface for WCAG. I like to use this page as a reference.
-- [Colour Blind Awareness](https://www.colourblindawareness.org/) - You can learn about different types of color blindness.
+{% set list = resources.knowledge %}
+{% for resource in list %}
+  {% include "resources/card.njk" %}
+{% endfor %}
 
 ## Videos
 
 You can get a lot of information from watching experts talk about web development.
 
-- [How To Get Hired as a Web Developer & WOW Hiring Managers + much more | AMA with Matt Studdert - YouTube](https://www.youtube.com/watch?v=EVrijj32mAc) - In this video, Matt is answering some incredible questions. I like his answer for the "how do you choose what to focus on learning?". He said that you should just focus on the next thing. For me, that answer put my worries to rest.
-- [Selfish Accessibility | Adrian Roselli | Talks at Google - YouTube](https://www.youtube.com/watch?v=XMAksFzFh9E) - This is a great video to help you aware of web accessibility. Also, you will learn different situations of users that want to access the web. Plus, how to do manual testing to check the accessibility of your website. I highly recommend watching it.
-- [Manuel Matuzović - Lost in Translation - YouTube](https://www.youtube.com/watch?v=Wno1IhEBTxc) - HTML is not as easy as you think.
-- [Andy Bell – Be the browser’s mentor, not its micromanager - YouTube](https://www.youtube.com/watch?v=5uhIiI9Ld5M) - A great talk by Andy Bell that will tell you that good HTML markup can be a strong foundation for your website. Then, how to write modern CSS with progressive enhancement.
-- [Stephanie Eeckles - Scaling CSS Layout Beyond Pixels - YouTube](https://www.youtube.com/watch?v=8slZJrTK3nE) - Another great talk that covers modern CSS. I recommend watching it to take your CSS skill to the next level.
-- [Resilient CSS: 7-part Series - YouTube](https://www.youtube.com/playlist?list=PLbSquHt1VCf1kpv9WRGMCA9_Nn4vCLZ9Y) - This series created by Jen Simmons taught me different ways to write CSS that will fail nicely. Also, a guide to using the "Can I Use" website to help me decide how I craft my CSS.
-- [Writing Less Damned Code – Heydon Pickering – btconfBER2016 - YouTube](https://www.youtube.com/watch?v=tzfHlEFd2Fk) - Less is less. Less code means less problems, less issues, and more time to spend for other things in life.
-- [Heydon Pickering - Why Inclusive Design is like cooking a curry - YouTube](https://www.youtube.com/watch?v=VavUglhLH8U) - Learning web accessibility can be like learning to cook a curry.
+{% set list = resources.videos %}
+{% for resource in list %}
+  {% include "resources/card.njk" %}
+{% endfor %}
 
 ## Tools
 
 You can make your development process better and quicker by using these tools.
 
-- [W3C HTML Validator](https://validator.w3.org/) - Accessible website starts with valid HTML. So, make sure that your website has valid HTML.
-- [The W3C CSS Validation Service](https://jigsaw.w3.org/css-validator/) - Valid CSS is also important to ensure that the styling of your site works as expected.
-- [W3C Feed Validation Service, for Atom and RSS](https://validator.w3.org/feed/) - I use this tool to validate my RSS feed.
-- [W3C Link Checker](https://validator.w3.org/checklink) - Make sure that you have valid links on your site.
-- [Schema Validator](https://validator.schema.org/) - Validate your schema data.
-- [Lighthouse](https://developer.chrome.com/docs/lighthouse/) - I recommend using this as the first automatic testing tool for your site. It can catch some accessibility issues and do different types of tests too.
-- [PageSpeed Insights](https://pagespeed.web.dev/) - You can use this website to share with people about your Lighthouse score.
-- [Squoosh](https://squoosh.app/) - Optimize your images so that your users will not be frustrated with their cellular data usage.
-- [Free Design Tool: Presentations, Video, Social Media | Canva](https://www.canva.com/) - I use this to design the banners for my READMEs and favicons.
-- [Grammarly: Free Online Writing Assistant](https://www.grammarly.com/) - This tool prevents me from doing typos in my writing.
-- [Favicon Generator for perfect icons on all browsers](https://realfavicongenerator.net/) - I use this tool to generate favicons for this website.
-- [RegExr: Learn, Build, & Test RegEx](https://regexr.com/) - Practice writing regular expression easily.
-- [CloudConvert](https://cloudconvert.com/) - I usually use this website to convert TTF (TrueType) to WOFF2 (Web Open Font Format 2). [TTF to WOFF2 | CloudConvert](https://cloudconvert.com/ttf-to-woff2)
-- [SVGOMG - SVGO's Missing GUI](https://jakearchibald.github.io/svgomg/) - Optimize your SVG files by removing unnecessary code.
-- [Colorable](https://colorable.jxnblk.com/) - Compare two colors to see the contrast of them.
-- [Hemingway Editor](https://hemingwayapp.com/) - It makes my writing clear and bold by highlighting potential words that can be removed.
-- [Caninclude](https://caninclude.glitch.me/) - If you get confused about whether `<a>` is valid to be a child element of the `<code>` element, then you can use this tool to verify that.
-- [Can I use... Support tables for HTML5, CSS3, etc](https://caniuse.com/) - Check the browser's compatibility with the features that you want to use.
-- [Accessibility Support](https://a11ysupport.io/) - Check the compatibility of screen readers with the ARIA attributes you want to use.
-- [Mailtolink.me | The Mailto Link Generator](https://mailtolink.me/) - Generate a `mailto` link easily.
+{% set list = resources.tools %}
+{% for resource in list %}
+  {% include "resources/card.njk" %}
+{% endfor %}
 
 ## Screen Readers
 
 It is beneficial to learn how to use screen readers to do manual testing on your website. Here are some videos to help.
 
-- [Screen Reader Basics: NVDA -- A11ycasts #09 - YouTube](https://youtu.be/Jao3s_CwdRU) - Learn how to use NVDA screen reader on Windows.
-- [Assistive Tech: TalkBack - YouTube](https://youtu.be/0Zpzl4EKCco) - Learn the basics of navigating with TalkBack.
-- [Complete guide to Narrator - Microsoft Support](https://support.microsoft.com/en-us/windows/complete-guide-to-narrator-e4397a0d-ef4f-b386-d8ae-c172f109bdb1) - Everything you need to know to start using Narrator.
-- [Assistive Tech: VoiceOver on iOS - YouTube](https://youtu.be/bCHpdjvxBws) - Learn to get started to use VoiceOver on iOS devices.
-- [Screen Reader Basics: VoiceOver -- A11ycasts #07 - YouTube](https://youtu.be/5R-6WvAihms) - Learn to use the free screen reader built into the Mac.
-- [Screen Reader Keyboard Shortcuts and Gestures](https://dequeuniversity.com/screenreaders/) - List of resources for various screen readers like Narrator, TalkBack, NVDA, and JAWS.
-- [Using NVDA to Evaluate Web Accessibility](https://webaim.org/articles/nvda/) - This is a great article to help you learn the basics of NVDA.
+{% set list = resources.screenReaders %}
+{% for resource in list %}
+  {% include "resources/card.njk" %}
+{% endfor %}
 
 ## Games
 
 Tired of coding? Why don't you try playing some of these games to relax?
 
-- Play Dino Game (chrome://dino) - This game shows when you are offline.
-- [HTML Tags Memory Test](https://codepen.io/plfstr/full/zYqQeRw) - The last time I played this, I think I got around 50 HTML elements. Can you beat mine?
-- [TypeRacer - Play Typing Games and Race Friends](https://play.typeracer.com/) - This is a place for me to type something other than code.
-- [dulvui/ball2box: Casual ball game created with Godot Engine.](https://github.com/dulvui/ball2box) - A game that has a purpose to put a ball inside a box. I have played 30 levels and got all three stars.
-- [Anuken/Mindustry: The automation tower defense RTS](https://github.com/Anuken/Mindustry) - An interesting tower defense where you have to gather resources and defense your base.
-- [agateau/pixelwheels: A top-down retro racing game for PC (Linux, macOS, Windows) and Android.](https://github.com/agateau/pixelwheels) - A great racing game with a simple controller and running smoothly on my low-end device. No in-app purchase. Meaning you can get all the cars by playing the game.
-- [mjaun/android-anuto: Another ugly tower defense for Android](https://github.com/mjaun/android-anuto) - I think it has a simple and good graphic. It has a small file size, only 6MB.
-
-<p class="time">
-  Last updated on
-  <time datetime="{{ page.date | htmlDateString }}"
-    >{{ page.date | readableDate }}</time
-  >.
-</p>
+{% set list = resources.games %}
+{% for resource in list %}
+  {% include "resources/card.njk" %}
+{% endfor %}
